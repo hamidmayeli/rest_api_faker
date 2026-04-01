f5cadccf620955ccaaa13c902380d12c27ce6f52
Hamid Mayeli <Hamid.Mayeli@yahoo.com>
2026-04-01 11:35:51 +0100

    Fix a couple of build errors

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts', 'src/cli.ts'],
   format: ['cjs'],
-  dts: true,
+  dts: { entry: ['src/index.ts'] },
   splitting: false,
   sourcemap: true,
   clean: true,
